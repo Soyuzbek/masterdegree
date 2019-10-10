593e103d572e8b2ad0392ea75d33c95b12035a8c
soyuzbek <soyuzbek196.kg@gmail.com>
2019-10-10 15:48:14 +0600

    10/10/19

--- modeling/homeworks/least_square_method.py
+++ modeling/homeworks/least_square_method.py
@@ -1,5 +1,9 @@
+import random
+
 import matplotlib.pyplot as plt
 import math
+
+from numpy import linspace
 
 """ My first homework on modeling subject"""
 
@@ -39,10 +43,24 @@ def gaussian(a, b):
 
 
 # first problem of first homework
-x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-y = [0.000269819, 0.20966649, 0.405653264, 0.600161378, 0.808358635, 1.000460357, 1.206855043, 1.400662266, 1.603810939,
-     1.807376137, 2.002681541]
+x = linspace(-5, 5, 20)
+y = [random.random()*100 for i in x]
 
+
+# x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+# y = [0.000269819, 0.20966649, 0.405653264, 0.600161378, 0.808358635, 1.000460357, 1.206855043, 1.400662266, 1.603810939,
+#      1.807376137, 2.002681541]
+# second problem of first homework
+# x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+# y = [
+#     5.007551402, 5.022739776, 5.084108155, 5.180491601, 5.325066589, 5.507693178, 5.725436069, 5.987606914, 6.289589143,
+#     6.628711888, 7.006814729]
+# third problem of first homework
+# x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+# initial_y = [1.011378034, 1.509673763, 2.2396243, 3.320296591, 4.95716554, 7.394593138, 11.02489662, 16.44550924,
+#              24.53945915, 36.61225519, 54.60938299]
+# y = [math.log(i, math.exp(1)) for i in initial_y]
+# first solution
 a = [
     [sum([i ** 2 for i in x]), sum(x)],
     [sum(x), len(x)]
@@ -50,11 +68,7 @@ a = [
 
 b = [[sum([i * j for i, j in zip(x, y)])], [sum(y)]]
 
-# second problem of first homework
-# x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-y = [
-    5.007551402, 5.022739776, 5.084108155, 5.180491601, 5.325066589, 5.507693178, 5.725436069, 5.987606914, 6.289589143,
-    6.628711888, 7.006814729]
+# second solution
 # a = [
 #     [sum([i ** 4 for i in x]), sum([i ** 3 for i in x]), sum([i ** 2 for i in x])],
 #     [sum([i ** 3 for i in x]), sum([i ** 2 for i in x]), sum(x)],
@@ -63,11 +77,7 @@ y = [
 #
 # b = [[sum([j * i ** 2 for i, j in zip(x, y)])], [sum([i * j for i, j in zip(x, y)])], [sum(y)]]
 
-# third problem of first homework
-# x = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
-# initial_y = [1.011378034, 1.509673763, 2.2396243, 3.320296591, 4.95716554, 7.394593138, 11.02489662, 16.44550924,
-#              24.53945915, 36.61225519, 54.60938299]
-# y = [math.log(i, math.exp(1)) for i in initial_y]
+# third solution
 # a = [
 #     [sum([i ** 2 for i in x]), sum(x)],
 #     [sum(x), len(x)]
@@ -79,19 +89,30 @@ solution = gaussian(a, b)  # the found x1, x2, ... , xn values
 print(solution)
 # first solution
 plt.plot(x, y, ' bo', label='initial')
-plt.plot(x, [solution[0] * i + solution[1] for i in x], label=r'${%f}x+{%f}$' % (solution[0], solution[1]))
-
+Y = [solution[0] * i + solution[1] for i in x]
+plt.plot(x, Y, label=r'${%f}x+{%f}$' % (solution[0], solution[1]))
 # second solution
-# plt.plot(x, y, ' o', label='initial')
-# plt.plot(x, [solution[0] * i ** 2 + solution[1] * i + solution[2] for i in x], '-rx',
+# Y = [solution[0] * i ** 2 + solution[1] * i + solution[2] for i in x]
+# plt.plot(x, Y, '-rx',
 #          label=r'${%f}x^2+{%f}x+{%f}$' % (solution[0], solution[1], solution[2]))
 
 # third
-# plt.plot(x, initial_y, ' bo', label='initial')
-# plt.plot(x, [math.exp(solution[1]) * math.exp(solution[0] * i) for i in x], '-rx',
+# Y = [math.exp(solution[1]) * math.exp(solution[0] * i) for i in x]
+# plt.plot(x, Y, '-rx',
 #          label=r'${%f}e^{%sx}$' % (solution[1], {solution[0]}))
+
+
+sigma = math.sqrt(sum([i ** 2 for i in y]) / len(y) - (sum(y) / len(y)) ** 2)
+epsilon = [math.fabs(i - j) for i, j in zip(y, Y)]
+print(f'epsilon: {epsilon}', f'sigma: {sigma}')
+m = 0
+indices = list(range(len(y)))
+for i in indices:
+    if epsilon[i] < sigma:
+        m += 1
+percent = (m * 100) / len(y)
 plt.xlabel('x label')
 plt.ylabel('y label')
-plt.title('least square method')
+plt.title(f'least square method, coincidence: {percent} sigma: {sigma}')
 plt.legend()
 plt.show()
